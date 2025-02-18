66370ede39b6f15ccebab65aa9c3eb0a46db32a3
Stephen Leach <sfkleach@gmail.com>
2025-02-18 13:41:41 +0000

    Presentation ready!

--- step_03_part_01.py
+++ step_03_part_01.py
@@ -2,22 +2,22 @@ import math
 import scipy.stats as stats
 
 def distance(point2d):
-	(x, y) = point2d
-	return math.sqrt(x*x + y*y)
+    (x, y) = point2d
+    return math.sqrt(x*x + y*y)
 
 def angle(point2d):
-	(x, y) = point2d
-	return math.atan2(y, x)
+    (x, y) = point2d
+    return math.atan2(y, x)
 
 def rotate(point2d, theta):
-	(x, y) = point2d
-	ct = math.cos(theta)
-	st = math.sin(theta)
-	return ct * x - st * y, st * x + ct * y
+    (x, y) = point2d
+    ct = math.cos(theta)
+    st = math.sin(theta)
+    return ct * x - st * y, st * x + ct * y
 
 def midpoint(point2d):
-	(x, y) = point2d
-	return 0.5 * x, 0.5 * y
+    (x, y) = point2d
+    return 0.5 * x, 0.5 * y
 
 def probability(ndist, x):
     return stats.norm.cdf(x, ndist[0], ndist[1])
--- step_03_part_02.py
+++ step_03_part_02.py
@@ -4,23 +4,23 @@ import scipy.stats as stats
 
 class Point2D:
 
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 def distance(p):
-	return math.sqrt(p.x*p.x + p.y*p.y)
+    return math.sqrt(p.x*p.x + p.y*p.y)
 
 def angle(p):
-	return math.atan2(p.y, p.x)
+    return math.atan2(p.y, p.x)
 
 def rotate(p, theta):
-	ct = math.cos(theta)
-	st = math.sin(theta)
-	return Point2D(ct * p.x - st * p.y, st * p.x + ct * p.y)
+    ct = math.cos(theta)
+    st = math.sin(theta)
+    return Point2D(ct * p.x - st * p.y, st * p.x + ct * p.y)
 
 def midpoint(p):
-	return 0.5 * p.x, 0.5 * p.y
+    return 0.5 * p.x, 0.5 * p.y
 
 
 class NormalDistribution:
